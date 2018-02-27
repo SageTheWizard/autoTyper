769d7b23eab1ed1c2e8145c418fb34c2a54f12b9
SageTheWizard <35324161+SageTheWizard@users.noreply.github.com>
2018-02-27 15:14:11 -0500

    Add files via upload
    Added Go Again option

--- autoTyper.java
+++ autoTyper.java
@@ -7,25 +7,37 @@ public class autoTyper
    public static void main(String[] args)
    {
       String text;
+	  char goAgain;
       int timeOfSpam;
       int delayTime;
           
       Scanner keyboard = new Scanner(System.in);
-      System.out.println("Jacob's really bad autotyper - CMD Edition!\n");
-      System.out.print("Input text you wish to be spammed: ");
-      text = keyboard.nextLine();
-      System.out.print("Enter number of times the text will be spammed: ");
-      timeOfSpam = keyboard.nextInt();
-      keyboard.nextLine();
-      System.out.print("Enter Delay between each spam message (in ms): ");
-      delayTime = keyboard.nextInt();
-      keyboard.nextLine();
       
-      System.out.println("Spam will begin upon pressing enter!");
-      keyboard.nextLine();
+	  do 
+	  {
+	  	System.out.println("Jacob's really bad autotyper - CMD Edition!\n");
+      	System.out.print("Input text you wish to be spammed: ");
+      	text = keyboard.nextLine();
+      	System.out.print("Enter number of times the text will be spammed: ");
+      	timeOfSpam = keyboard.nextInt();
+      	keyboard.nextLine();
+      	System.out.print("Enter Delay between each spam message (in ms): ");
+      	delayTime = keyboard.nextInt();
+      	keyboard.nextLine();
       
-      beginSpam(text, timeOfSpam, delayTime);
-      System.exit(0);
+        System.out.println("Spam will begin upon pressing enter!");
+      	keyboard.nextLine();
+      
+      	beginSpam(text, timeOfSpam, delayTime);
+		System.out.print("Would you like to go again?(Y/N): ");
+	    do 
+		{	
+			goAgain = ((keyboard.nextLine()).toUpperCase()).charAt(0);
+			if ((goAgain != 'Y') & (goAgain != 'N'));
+				System.out.print("Please Enter Y(es) or N(o): "); 
+		}while ((goAgain != 'Y') & (goAgain != 'N'));
+      } while (goAgain == 'Y');
+	  System.exit(0);
     }
     
     private static void beginSpam(String spamText, int timeOfSpam, int delayTime)
